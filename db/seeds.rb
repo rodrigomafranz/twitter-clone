befa25f0fdc1a15784f003ec87946505c61f1f0d
Rodrigo Alexandre Mafra <rodrigomafranz@gmail.com>
2023-06-10 20:40:58 -0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development? && User.count.zero? && Message.count.zero?
+	users = 5.times.map do |i|
+		User.create(name: "User #{i + 1}", email: "user#{i + 1}@example.com", password: '12345678')
+	end
+
+	100.times do |i|
+		image_url = ["https://picsum.photos/id/#{rand(0..1084)}/300", nil].sample
+		users.sample.messages.create(text: Faker::ChuckNorris.fact, image_url)
+	end
+end
